748fa00064a49835ecfa860657cd3636a82a8c99
Joy Huang <joyhuang@kklab.com>
2023-04-07 16:06:40 +0800

    Add thumbnail in Post model

--- posts/models.py
+++ posts/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     body = models.CharField(max_length=100000, default='')
     author = models.CharField(max_length=100, default='Unknown author')
     created_at = models.DateTimeField(default=datetime.now, blank=True)
+    thumbnail = models.ImageField(upload_to='uploads', default='')
 
     def __str__(self):
         return self.title
--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from blog import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/<int:post_id>', views.posts, name='posts'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
