363c662d6c13b7cc77e94af9668fabd1876e42b9
Joy Huang <joyhuang@kklab.com>
2023-04-07 14:06:36 +0800

    Add Post model and pass data to view

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, default='Untitled post')
+    body = models.CharField(max_length=100000, default='')
+    author = models.CharField(max_length=100, default='Unknown author')
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
